d2dcf46bbf9c30891561ddd8696fbcd36b43aa57
Kyndall Holt <kyngrace@ad.unc.edu>
2023-01-30 20:24:49 -0500

    ex02_one_shot_wordle

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,4 +2,15 @@
 __author__ = "730553137"
 
 secret_word: str = "python"
-print("What is your 6-letter guess? ")
+
+secret_word_idx: int = 0
+
+input("What is your 6-letter guess? ")
+while secret_word_idx != len(secret_word):
+    input("That was not 6 letters! Try again: ")
+    secret_word_idx = secret_word_idx +1
+while secret_word_idx == len(secret_word):
+    if secret_word == secret_word:
+        print("Woo! You got it!")
+else:
+    print("Not quite. Play again soon!")
