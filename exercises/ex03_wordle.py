c22744a46a2fd3c4ad40c9e2ae96a17c0dcee564
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-07 21:11:20 -0500

    ex03_wordle

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,3 +1,12 @@
 """EX03 - Wordle - Make your best guess!"""
 __author__ = "730553137"
 
+being_searched_idx: int = 0
+def contains_char(being_searched: str, being_found: str) -> bool:
+    """Returns a character that is being searched for"""
+    assert len(being_found) == 1
+    while being_searched_idx == being_found:
+        if being_searched_idx == being_found:
+            return True
+        else: #being_searched_idx != being_found
+            return False
