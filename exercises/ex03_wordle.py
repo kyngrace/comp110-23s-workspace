06c61684b82972b28f2701dc54338fb70b91f09a
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-16 18:27:07 -0500

    progress on ex03_wordle

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,16 +1,45 @@
 """EX03 - Wordle - Make your best guess!"""
 __author__ = "730553137"
 
-being_searched: str = "abc"
-being_searched_idx: int = 0
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
-def contains_char(being_searched: str, being_found: str) -> bool:
+def contains_char(word: str, character: str) -> bool:
     """Returns a character that is being searched for"""
-    assert len(being_found) == 1
-    while being_searched_idx == being_found:
-        if being_searched_idx == being_found:
+    word_idx: int = 0
+
+    assert len(character) == 1
+    while word_idx < len(word):
+        if word[word_idx] == character:
             return True
-        else: #being_searched_idx != being_found
-            return False       
         
-being_searched_idx = being_searched_idx + 1
+        word_idx += 1
+
+    return False    
+
+def emojified(guess: str, secret: str) -> str: 
+    """Returns a string of emoji based on a characters appearance"""
+
+    secret_idx: int = 0
+    emoji: str = ""
+    assert len(guess) == len(secret)
+    while secret_idx < len(secret):
+        if guess[secret_idx] == secret[secret_idx]:
+            emoji += GREEN_BOX
+        else: 
+            does_word_exist: bool = contains_char(secret, guess[secret_idx])
+            
+            if does_word_exist:
+                emoji += YELLOW_BOX
+            else:
+                emoji += WHITE_BOX
+        secret_idx = secret_idx + 1
+    return emoji
+
+def input_guess(expected_length: int) -> str:
+    """Prompts user for a guess until they provide one of the correct length"""
+    guess = input(f"Enter a {expected_length} character word: ")
+    while len(guess) != expected_length:
+        guess = input(f"That wasn't {expected_length} chars! Try again: ")
+    return guess 
