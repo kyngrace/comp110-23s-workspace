e22687d7afbbb352cbe8da7e9652f6630408ee78
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-21 13:01:35 -0500

    ex04_utils progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,11 +1,18 @@
-"""EX04 - """
+"""EX04 - 'list' Utility Functions."""
 __author__ = "730553137"
 
 def all(int_list: list[str], matching_int: int) -> bool:
     """Returns a bool that indicates if all of the ints in the list are the same as the given int."""
-    if len(int_list) == 0:
-        return False
+
+    int_list_idx: int = 0
+    while int_list_idx < len(int_list):
+        matching_int = int_list[0]
+        int_list_idx += 1  
     if matching_int == int_list:
         return True
     else: 
         return False
+    if len(int_list) == 0:
+        return False
+    
+    
