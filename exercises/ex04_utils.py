cf30df1bee4e84a599b9804890be7a5d5aa3b29b
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-23 15:54:53 -0500

    ex04_utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,7 +5,6 @@ __author__ = "730553137"
 def all(int_list: list[int], matching_int: int) -> bool:
     # declares the function and names the parameters
     """Returns a bool that indicates if all of the ints in the list are the same as the given int."""
-
     int_list_idx: int = 0
     # assigns the int_list_idx variable to track the index of the current int in the int_list
     while int_list_idx < len(int_list):
@@ -27,36 +26,34 @@ def all(int_list: list[int], matching_int: int) -> bool:
 def max(input: list[int]) -> int:
     # declares the function and names the parameter
    """Returns the maximum integer from a given list or returns a ValueError for an empty list."""
-
    if len(input) == 0:
     # check if the input list is empty
        raise ValueError("max() arg is an empty List")
        # raise a ValueError with a specific error message
-   max = input[0]
-   # initialize the maximum number to the first number in the list
-   counter = 1
-   # create a counter variable and initialize it to 1
+   max_int: int = input[0]
+   # initialize the maximum integer to the first integer in the list
+   counter: int = 0
+   # create a counter variable and initialize it to 0
    while counter < len(input):
     # check if counter is less than the length of the input list
-       if input[counter] > max:
+       if input[counter] > max_int:
         # check if the current integer being checked is larger
-           max = input[counter]
+           max_int = input[counter]
            # update the maximum integer
        counter += 1
        # counter variable moves on to the next integer in the list
-   return max
+   return max_int
    # return the value of the final maximum integer
 
 
 def is_equal(int_list1: list[int], int_list2: list[int]) -> bool:
     # declares the function and names the parameters
-   """Returns a bool that indicates if every element at every index is equal in both lists."""
-   
+   """Returns a bool that indicates if every element at every index is equal in both lists."""   
    if len(int_list1) != len(int_list2):
     # check if the lengths of int_list1 and int_list2 are equal
        return False
        # return False if the lengths of the two lists are not equal
-   idx = 0
+   idx: int = 0
    # initialize an indexing variable
    while idx < len(int_list1):
     # compare the integers in both of the lists at a certain index
