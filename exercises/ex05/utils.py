e20e7f8c52dfe99e823733d47a7d5d17f48c658c
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-28 13:17:43 -0500

    ex05 progress

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,6 @@
+"""EX05 - Working with lists and unit tests."""
+__author__ = "730553137"
+
+def only_evens(even_list: list[int]) -> list[int]:
+    """Returns a list from the input list with only the even numbers."""
+   
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,2 @@
+"""EX05 - Working with lists and unit tests."""
+__author__ = "730553137"
