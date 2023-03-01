a2568e53eb71e81451a5aaa01127e65bd833002f
Kyndall Holt <kyngrace@ad.unc.edu>
2023-02-28 22:42:06 -0500

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,6 +1,16 @@
 """EX05 - Working with lists and unit tests."""
 __author__ = "730553137"
 
-def only_evens(even_list: list[int]) -> list[int]:
+def only_evens(given_list: list[int]) -> list[int]:
+    # declares the function and names the parameter
     """Returns a list from the input list with only the even numbers."""
-   
+    even_list: list[int] = []
+    # assigns the even_list variable and creates an empty list to keep even integers
+    for int in given_list:
+        # creates a for...in loop to iterate through each integer in the given list
+        if int % 2 == 0:
+            # check if the integer is even
+            even_list.append(int)
+            # if the integer is even then add it to the outputed even list
+    return even_list
+    # return the new created list that contains only even integers
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,2 +1,19 @@
 """EX05 - Working with lists and unit tests."""
 __author__ = "730553137"
+
+from exercises.ex05.utils import only_evens
+
+def test_only_evens_empty_list() -> None:
+    # one edge case test
+    given_list: list[int] = []
+    assert only_evens(given_list) == []
+
+def test_only_evens_with_mixed_list() -> None:
+    # one use case test
+    given_list: list[int] = [1, 4, 5, 6, 8, 9]
+    assert only_evens(given_list) == [4, 6, 8]
+
+def test_only_evens_with_odd_list() -> None:
+    # one use case test
+    given_list: list[int] = [3, 5, 7, 11, 15]
+    assert only_evens(given_list) == []
