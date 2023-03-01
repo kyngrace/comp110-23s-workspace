9139dbbbb0b913011cc57509c44104af56d2b0c7
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-01 09:22:57 -0500

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -22,3 +22,7 @@ def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     # assigns the combined_list variable that is equal to the first_list and second_list combined
     return combined_list
     # returns the new created list that contains both the first_list and second_list
+
+def sub(int_list: list[int], start_idx: int, end_idx: int) -> list[int]:
+    """Returns a list that is a subset of the input list that stays between the start_idx and the end_idx."""
+    
