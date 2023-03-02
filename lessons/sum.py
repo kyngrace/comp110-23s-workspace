8540a7975b35370791511a01aa22db81bdc8e7d3
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-02 08:58:57 -0500

    sum.py for...in loop practice

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,7 +2,22 @@
 
 def sum(xs: list[float]) -> float:
     """return sum of all elements in xs"""
+    # while loop
     sum_total: float = 0.0
-    for element in xs:
-        sum_total += element
+    idx: int = 0
+    while idx < len(xs):
+        sum_total += xs[idx]
+        idx += 1
+    return sum_total
+
+    # for...in loop
+    sum_total: float = 0.0
+    for elem in xs:
+        sum_total += elem
+    return sum_total
+
+    # for...in...range loop
+    sum_total: float = 0.0
+    for idx in range(len(xs)):
+        sum_total += xs[idx]
     return sum_total
