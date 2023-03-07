49609aa45754e541bde3b511aeaf4dac53c29954
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-07 13:32:01 -0500

    ex06 progress

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -1,8 +1,13 @@
 """EX06 - Taking care of my plant!"""
 __author__ = "730553137"
 
+WATER_EMOJI: str = "\U0001F4A7"
+FIRE_EMOJI: str = "\U0001F525"
+PLANT_EMOJI: str = "\U0001F331"
+
+
 def main() -> None:
-    print("Welcome to my program!")
+    greet()
     
     global points
     points: int = 0
@@ -11,21 +16,24 @@ def main() -> None:
     player: str = ""
 
 
-def greet(player) -> None:
-    print(f"Hello {player}, welcome to my garden!")
+def greet(player: str) -> None:
+    print(f"Welcome to my {PLANT_EMOJI} garden game!")
+    player: str = input("What's your name? ")
+    print(f"Hello {player}! I hope you're ready to take care of your plant!")
 
 
-def first_option(total):
-    print(f"You picked the end option. Goodbye! You accumulated {total} adventure points.")
+def first_option(points: int):
+    print(f"You picked the end game option. Goodbye! You accumulated {points} adventure points.")
 
 
-def second_option():
-    print("You picked the second option. You are headed to the garden to choose an already grown plant!")
+def second_option(points: int):
+    print(f"You picked the second option. You get to {WATER_EMOJI} water your plant. Your plant is happy!")
+    points += 5
 
 
-def third_option():
-    print("You picked the third option. You are headed to the store to get seeds and start from scratch!")
-
+def third_option(points: int):
+    print(f"You picked the third option. You set one of your plants leaves on {FIRE_EMOJI} fire. Your plant is not happy!")
+    points -= 5
 
 if __name__ == "__main__":
     main()
