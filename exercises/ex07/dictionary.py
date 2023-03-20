b5bfcfebd86b70c0b6355221fbdd431f039d7e2d
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-20 14:41:07 -0400

    ex07 progress

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,7 @@
+"""EX07 - Working with dictionary utilities."""
+__author__ = "730553137"
+
+
+def invert(given_list: dict[str, str]) -> dict[str, str]: 
+    """Returns a list that swaps the values and keys between the given and output list."""
+  
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,2 @@
+"""EX07 - Working with dictionary utilities."""
+__author__ = "730553137"
