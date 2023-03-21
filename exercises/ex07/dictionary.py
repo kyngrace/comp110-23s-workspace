bb58e184b5243d9f4fc67d72554871cbdde8efdc
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-20 23:43:02 -0400

    ex07 progress

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -2,6 +2,21 @@
 __author__ = "730553137"
 
 
-def invert(given_list: dict[str, str]) -> dict[str, str]: 
-    """Returns a list that swaps the values and keys between the given and output list."""
-  
+def invert(given_dict: dict[str, str]) -> dict[str, list[str]]:
+    # declares the function and names the parameter 
+    """Returns a dictionary that swaps the values and keys between the given and output list."""
+    inverted_dict: dict[str, list[str]] = {}
+    # initializes the inverted_dict variable to an empty dictionary to hold the inverted keys and values
+    for key in given_dict:
+        # creates a for loop that loops through each key in the given dictionary 
+        value: str = given_dict[key]
+        # initializes the value variable in order to make it equal to the current key being looped through in the dictionary 
+        if value in inverted_dict:
+            # checks if the value already exists as a key once inverted 
+            raise KeyError(f"Error! {value} already exists in the inverted dictionary as a key.")
+                # raises a KeyError
+        else:
+            # if it gets to this point in the loop then the key/value pair is added to the inverted dictionary
+            inverted_dict[value] = [key]
+    return inverted_dict
+        # returns the inverted dictionary 
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,2 +1,25 @@
 """EX07 - Working with dictionary utilities."""
 __author__ = "730553137"
+
+from exercises.ex07.dictionary import invert
+
+
+def test_invert_empty_dict() -> None:
+    """This function tests if the invert function will return an empty dictionary if the given dictionary is empty."""
+    # one edge case test
+    given_dict: dict[str, str] = {}
+    assert invert(given_dict) == {}
+
+
+def test_invert_expected_output() -> None:
+    """This function tests if the invert function will return the expected output based on a given dictionary."""
+    # one use case test
+    given_dict: dict[str, str] = {"b": "2", "d": "4", "f": "6"}
+    expected_output: dict[str, str] = {"2": ["b"], "4": ["d"], "6": ["f"]}
+    assert invert(given_dict) == expected_output
+
+
+def test_invert_easy_input() -> None:
+    """This function tests if the invert function will return an output correctly based on a simple input."""
+    # one use case test 
+    assert invert({"t": "2", "f": "5", "c": "6"}) == {"2": ["t"], "5": ["f"], "6": ["c"]}
