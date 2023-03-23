7fd5b5404b5cb7e842da9b1e134b5e334e1b17e9
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-23 15:20:43 -0400

    exo7 progress

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -2,10 +2,10 @@
 __author__ = "730553137"
 
 
-def invert(given_dict: dict[str, str]) -> dict[str, list[str]]:
+def invert(given_dict: dict[str, str]) -> dict[str, str]:
     # declares the function and names the parameter 
     """Returns a dictionary that swaps the values and keys between the given and output list."""
-    inverted_dict: dict[str, list[str]] = {}
+    inverted_dict: dict[str, str] = {}
     # initializes the inverted_dict variable to an empty dictionary to hold the inverted keys and values
     for key in given_dict:
         # creates a for loop that loops through each key in the given dictionary 
@@ -14,10 +14,10 @@ def invert(given_dict: dict[str, str]) -> dict[str, list[str]]:
         if value in inverted_dict:
             # checks if the value already exists as a key once inverted 
             raise KeyError(f"Error! {value} already exists in the inverted dictionary as a key.")
-                # raises a KeyError
         else:
             # if it gets to this point in the loop then the key/value pair is added to the inverted dictionary
-            inverted_dict[value] = [key]
+            inverted_dict[value] = key
+                # raises a KeyError
     return inverted_dict
         # returns the inverted dictionary
 
