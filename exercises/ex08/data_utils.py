a7b234c902f0a27994d618576964f40ed981d6b0
Kyndall Holt <kyngrace@ad.unc.edu>
2023-03-30 16:50:29 -0400

    ex08 progress

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,9 +1,9 @@
 from csv import DictReader
 
 
-def read_csv_rows(filename: str) -> list[dict[str,str]]:
-    """Read csv file and return as a list of dicts with header keys"""
-    result: list[dict[str,str]] = []
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read csv file and return as a list of dicts with header keys."""
+    result: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
@@ -12,21 +12,21 @@ def read_csv_rows(filename: str) -> list[dict[str,str]]:
     return result
 
 
-def column_values(table: list[dict[str,str]], header: str) -> list[str]:
-    """Returns values in a table column under a specific header"""
+def column_values(table: list[dict[str, str]], header: str) -> list[str]:
+    """Returns values in a table column under a specific header."""
     result: list[str] = []
-    #step through table
+    # step through table
     for row in table:
-        #save every value under key "header"
+        # save every value under key "header"
         result.append(row[header])
     return result
 
 
-def columnar(table: list[dict[str,str]]) -> dict[str,list[str]]:
-    """Reformats data so that it's a dictionary with column headers as keys"""
-    result: dict[str,list[str]] = {}
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Reformats data so that it's a dictionary with column headers as keys."""
+    result: dict[str, list[str]] = {}
     # loop through keys of one row of table
-    first_row: dict[str,list[str]] = table[0]
+    first_row: dict[str, list[str]] = table[0]
     for key in first_row:
         # for each key, make a dictionary entry with all column values
         result[key] = column_values(table, key)
@@ -46,7 +46,7 @@ def head(column_table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
             # Loops through first N items of the tables column
             if idx < len(column_table[column]):
                 # Checks if the column has more than idx values, append the value to the first_N list 
-                first_N.append(column_table[column].pop(0))
+                first_N.append(column_table[column][idx])
                 # Removes and append the first elem of the current column's list to the first_N list
                 new_table[column] = first_N
                 # Adds the resulting list to the dictionary that will be output 
